cdee072e9f624e98aee7c47ec123ac717663141b
Danai Adkisson <dadkisso@live.com>
2015-06-10 08:42:59 -0400

    fixed navigation

---
+++ README.md
@@ -0,0 +1,7 @@
+#Teacher App
+
+###Overview
+
+This program stores information about students, parents, and parent email in a teachers class. This program requires the teacher to log in with their email and a password. Once logged in the teacher can only view the students in his/her class. The teacher can add, destroy, or edit parts of the table. The teacher may also edit his/her name, email, and change password.
+
+Oh yeah and all the test fail.
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,20 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+array = [1,2,3,4,5]
+
 Educator.create!(name: "Seth Ward", email: "sward@uwyo.edu", password: "password")
+Educator.create!(name: "Steve Barnhart", email: "sbarnhart@uwyo.edu", password: "password")
+Educator.create!(name: "Daniel Blair", email: "dblair@uwyo.edu", password: "dog")
+Educator.create!(name: "Leonard Wahsington", email: "lwashington@uwyo.edu", password: "password")
 Educator.create!(name: "Danai Adkisson", email: "dadkisso@uwyo.edu", password: "danai")
 
-Parent.create!(student_name: "ddd", parent_name:"fff", parent_email:"sss", educator_id:1)
-Parent.create!(student_name: "lll", parent_name:"ggg", parent_email:"aaa", educator_id:2)
+
+
+50.times do
+  last_name = Faker::Name.last_name
+Parent.create!(student_name: "#{Faker::Name.first_name} #{last_name}",
+parent_name:"#{Faker::Name.first_name} #{last_name}", parent_email:"#{Faker::Internet.email}",
+educator_id: array.sample)
+
+end
